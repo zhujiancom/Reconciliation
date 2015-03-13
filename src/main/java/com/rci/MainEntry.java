3eb2364c6fa8db53b064735a8b52c541cae953aa
zhujiancom <eric87com@gmail.com>
2015-03-13 08:46:53 +0800

    666

--- src/main/java/com/rci/MainEntry.java
+++ src/main/java/com/rci/MainEntry.java
@@ -2,20 +2,21 @@ package com.rci;
 
 import javax.swing.JFrame;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.rci.ui.swing.MainFrame;
 
-
 public class MainEntry {
+
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-common.xml","spring/spring-db.xml");
+		new ClassPathXmlApplicationContext("spring/spring-common.xml",
+				"spring/spring-db.xml");
 		JFrame frame = new MainFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null); // 相对居中, 在frame设置size之后
 		frame.setVisible(true);
-//		frame.pack();
+		// frame.pack();
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 	}
 
